92290e65b826b520056e1477eca0d9b95c7c4ed5
nhattrinh17 <trinhminhnhatxt123@gmail.com>
2024-07-22 19:17:50 +0700

    update ......

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_URL: "http://localhost:9999/api",
+    // API_URL: "http://localhost:9999/api",
     // Product
-    // API_URL: "https://be.trinhminhnhat.info.vn/api",
+    API_URL: "https://be.trinhminhnhat.info.vn/api",
   },
   images: {
-    // remotePatterns: [
-    //   //
-    //   { hostname: "storage.googleapis.com" },
-    //   // { hostname: "d1k2oi80tv211b.cloudfront.net" },
-    // ],
-    domains: ["storage.googleapis.com"],
+    remotePatterns: [
+      //
+      { hostname: "storage.googleapis.com" },
+      // { hostname: "d1k2oi80tv211b.cloudfront.net" },
+    ],
+    // domains: ["storage.googleapis.com"],
     unoptimized: true,
   },
 };
