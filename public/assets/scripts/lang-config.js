9dfd03bba21c8bedcfd651577e5b244225329414
nhattrinh17 <trinhminhnhatxt123@gmail.com>
2024-07-23 11:19:29 +0700

    update language

--- public/assets/scripts/lang-config.js
+++ public/assets/scripts/lang-config.js
@@ -5,6 +5,7 @@ window.__GOOGLE_TRANSLATION_CONFIG__ = {
     { title: "Korean", name: "ko", positionY: -151 },
     { title: "Chinese", name: "zh-CN", positionY: -200 },
     { title: "Japanese", name: "ja", positionY: -176 },
+    { title: "German", name: "de", positionY: -78 },
   ],
   defaultLanguage: "en",
 };
