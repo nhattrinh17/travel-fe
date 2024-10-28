54e8ea32e3ac0de3a6633e93c517695008a7b396
nhattrinh17 <trinhminhnhatxt123@gmail.com>
2024-10-28 14:06:35 +0700

    update seo

--- src/app/destination/[slug]/page.tsx
+++ src/app/destination/[slug]/page.tsx
@@ -1,4 +1,58 @@
 import { DetailDestinationSection } from "@/sections/DetailDestination";
+import { Metadata, ResolvingMetadata } from "next";
+
+type Props = {
+  params: { slug: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+// Hàm generateMetadata để tạo metadata động cho trang
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const slug = params.slug;
+  const res = await fetch(
+    `${process.env.API_URL}/destination/${slug}/seo`
+  ).then((res) => res.json());
+
+  let destinationBrief = res?.data;
+  // Get data detail location when not found destination
+  if (!destinationBrief) {
+    const resLocation = await fetch(
+      `${process.env.API_URL}/detail-location/${slug}/seo`
+    ).then((res) => res.json());
+    destinationBrief = resLocation?.data;
+  }
+
+  if (!destinationBrief) {
+    return {
+      title: "TH Global Trave Destination",
+      openGraph: {
+        title: "TH Global Trave Destination",
+        description: "No details available",
+        images: [], // Không có hình ảnh
+        url: `${process.env.URL_MAIN}/blog/${slug}`,
+      },
+    };
+  }
+
+  const images = destinationBrief?.images
+    ? destinationBrief?.images?.split("*_*")
+    : [destinationBrief?.image];
+
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: destinationBrief?.name || "TH Global Trave Destination",
+    openGraph: {
+      title: destinationBrief?.name || "TH Global Trave Destination",
+      description: destinationBrief?.title || "No description available",
+      images: [...images, ...previousImages],
+      url: `${process.env.URL_MAIN}/destination/${slug}`,
+    },
+  };
+}
 
 export default function DestinationCruisePage({
   params,
