445e43f829e062f783af485b8f97d5f87863d87f
nhattrinh17 <trinhminhnhatxt123@gmail.com>
2024-11-20 10:11:27 +0700

    add git action

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,6 +47,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Script id="google-analytics" strategy="beforeInteractive">
+          {`
+           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NJ5PL7W3')
+          `}
+        </Script>
         <Script
           src="/assets/scripts/lang-config.js"
           strategy="beforeInteractive"
@@ -74,6 +83,14 @@ export default function RootLayout({
         </Script>
       </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NJ5PL7W3"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <StoreProvider>
           <MainLayout>{children}</MainLayout>
         </StoreProvider>
